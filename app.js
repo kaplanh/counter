fa6654685c925113ee94b13c7e477dc39fd89e05
kplnhsyn <kplnhsyn.49@gmail.com>
2023-06-11 20:10:30 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const p = document.getElementById("par");
+
+let dakika = +prompt("lütfen dakika giriniz");
+let saniye = dakika * 60;
+// console.log(saniye);
+function myFunction() {
+  let minutes = Math.floor(saniye / 60);
+  let seconds = saniye % 60;
+
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  // console.log(minutes);
+  p.innerText = `${minutes}:${seconds}`;
+  saniye--;
+
+  if (minutes >= 0) {
+    p.innerText = `${minutes}:${seconds}`;
+  } else {
+    p.innerText = "00:00";
+  }
+}
+myFunction();
+setInterval(myFunction, 1000);
