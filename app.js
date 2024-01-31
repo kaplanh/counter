ad8aaf1b9a1f7e8735078b86644cc1f0b8260457
kplnhsyn <kplnhsyn.49@gmail.com>
2024-01-31 23:51:49 +0100

    app.js updated

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-
-
-
-
-
 const p = document.getElementById("par");
+let dakika;
 
-let dakika = +prompt("lütfen dakika giriniz");
+const dakikaFunk = () => {
+    dakika = +prompt("lütfen dakika giriniz");
+};
+setTimeout(dakikaFunk, 3000);
+
 let saniye = dakika * 60;
 // console.log(saniye);
 function myFunction() {
-  let minutes = Math.floor(saniye / 60);
-  let seconds = saniye % 60;
+    let minutes = Math.floor(saniye / 60);
+    let seconds = saniye % 60;
 
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-  // console.log(minutes);
-  p.innerText = `${minutes}:${seconds}`;
-  saniye--;
-
-  if (minutes >= 0) {
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    // console.log(minutes);
     p.innerText = `${minutes}:${seconds}`;
-  } else {
-    p.innerText = "00:00";
-  }
+    saniye--;
+
+    if (minutes >= 0) {
+        p.innerText = `${minutes}:${seconds}`;
+    } else {
+        p.innerText = "00:00";
+    }
 }
 myFunction();
 setInterval(myFunction, 1000);
